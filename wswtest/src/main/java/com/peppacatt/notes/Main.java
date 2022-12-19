d73a2ff2ed5594b5d468647de63cac6b5c8a32f9
peppacatt <peppacatt@163.com>
2022-12-19 23:35:56 +0800

    时间复杂度

--- wswtest/src/main/java/com/peppacatt/notes/Main.java
+++ wswtest/src/main/java/com/peppacatt/notes/Main.java
@@ -1,11 +1,9 @@
 package com.peppacatt.notes;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int[] arr = {1, 2, 4, 7, 8, 9};
-        System.out.println(Arrays.binarySearch(arr, 7));
+        int n = 100;
+        int i = n + 2;
+        System.out.println(i);
     }
 }
