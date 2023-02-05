8b3825cd3eaa6924438c46e3b3a8559832db7a1c
peppacatt <peppacatt@163.com>
2023-02-06 00:34:19 +0800

    byte数组转为16进制

--- wswtest/src/main/java/com/peppacatt/notes/wswtest/Main.java
+++ wswtest/src/main/java/com/peppacatt/notes/wswtest/Main.java
@@ -2,8 +2,8 @@ package com.peppacatt.notes.wswtest;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 100;
-        int i = n + 2;
-        System.out.println(i);
+        for (int i = 0; i < 20; i++) {
+            System.out.printf("2^%d: %f%n", i, Math.pow(2, i));
+        }
     }
 }
