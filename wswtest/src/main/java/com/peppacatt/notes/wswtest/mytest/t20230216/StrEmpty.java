94c6efa7318da029a3ebd2768464124640f9e83b
peppacatt <peppacatt@163.com>
2023-03-06 22:15:50 +0800

    虚拟机栈

--- wswtest/src/main/java/com/peppacatt/notes/wswtest/mytest/t20230216/StrEmpty.java
+++ wswtest/src/main/java/com/peppacatt/notes/wswtest/mytest/t20230216/StrEmpty.java
@@ -1,16 +1,22 @@
 package com.peppacatt.notes.wswtest.mytest.t20230216;
 
-import cn.hutool.core.util.StrUtil;
-
 public class StrEmpty {
+    private int count = 0;
+
     public static void main(String[] args) {
-        System.out.println(StrUtil.isEmpty(""));
-        System.out.println(StrUtil.isEmpty(" "));
-        System.out.println(StrUtil.isEmpty("  "));
-        System.out.println(StrUtil.isEmpty(null));
-        System.out.println(StrUtil.isBlank(""));
-        System.out.println(StrUtil.isBlank(" "));
-        System.out.println(StrUtil.isBlank("  "));
-        System.out.println(StrUtil.isBlank(null));
+        m(1, 2);
+        new StrEmpty().m1();
+    }
+
+    private static void m(int a, int b) {
+        int c = 3;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+
+    private void m1() {
+        int a = 1;
+        int sum = a + this.count;
+        System.out.println(sum);
     }
 }
