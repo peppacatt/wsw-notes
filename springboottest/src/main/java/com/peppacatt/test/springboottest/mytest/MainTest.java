a647de95dd1b298e5de207c54de4123066a9eb8e
peppacatt <peppacatt@163.com>
2023-11-26 23:09:47 +0800

    数组

---
+++ springboottest/src/main/java/com/peppacatt/test/springboottest/mytest/MainTest.java
@@ -0,0 +1,8 @@
+package com.peppacatt.test.springboottest.mytest;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+    }
+}
