a197a1c332d5e27a331096c099c606f3528fc289
peppacatt <peppacatt@163.com>
2023-11-27 23:28:12 +0800

    c p53
    python
    p14

--- springboottest/src/main/java/com/peppacatt/test/springboottest/mytest/MainTest.java
+++ springboottest/src/main/java/com/peppacatt/test/springboottest/mytest/MainTest.java
@@ -2,7 +2,10 @@ package com.peppacatt.test.springboottest.mytest;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println(Byte.MIN_VALUE);
-        System.out.println(Byte.MAX_VALUE);
+        char x1 = 'a';
+        char x2 = 'A';
+        char x3 = '0';
+        System.out.printf("%d %d %d\n", (int) x1, (int) x2, (int) x3);
+        System.out.println((char) (x1 + 1));
     }
 }
