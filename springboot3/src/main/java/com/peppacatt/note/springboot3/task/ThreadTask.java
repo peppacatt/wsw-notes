6dc9c0045b871153c44cc3933194b741ec1c1e49
peppacatt <peppacatt@163.com>
2024-06-04 00:38:56 +0800

    rabbitmq

--- springboot3/src/main/java/com/peppacatt/note/springboot3/task/ThreadTask.java
+++ springboot3/src/main/java/com/peppacatt/note/springboot3/task/ThreadTask.java
@@ -1,11 +1,20 @@
 package com.peppacatt.note.springboot3.task;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class ThreadTask {
-    public void xxx(){
-        System.out.println("当前任务执行了");
-        System.out.println(Thread.currentThread());
+    @Scheduled(fixedRate = 5000)
+    public void aaa() throws InterruptedException {
+        Thread.sleep(10000);
+        System.out.println("aaa执行---" + Thread.currentThread() + "---" + new Date());
+    }
+
+    @Scheduled(fixedRate = 6000)
+    public void bbb() throws InterruptedException {
+        System.out.println("bbb执行---" + Thread.currentThread() + "---" + new Date());
     }
 }
